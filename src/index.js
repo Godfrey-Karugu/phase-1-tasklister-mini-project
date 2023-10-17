8265c246bac626cf41892ea0719a5aa208abf56f
Godfrey Karugu <karugugk@gmail.com>
2023-10-17 17:44:14 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', e =>{
+    e.preventDefault()
+    handleToDo(e.target.new_task_description.value)
+    })
+  
 });
+
+function handleToDo(todo){
+  let p = document.createElement('p')
+  p.textContent = todo;
+  let btn = document.createElement('button')
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = 'X'
+  p.appendChild(btn)
+  
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentElement.remove()
+}
